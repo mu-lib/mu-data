463124a20260ee6e2ba8240d81a41a36f9163036
Terrill Ye <terrill.ye@ef.com>
2015-04-20 04:17:38 -0400

    add test case for single quote inside double quote

--- test/query/parse-test.js
+++ test/query/parse-test.js
@@ -190,6 +190,16 @@ define([ "../../query/parse" ], function (parse) {
 		        "raw": "test!\"123 321\""
 		    }]);
 		},
+		
+		"test!\"'123 321'\"": function () {
+		    var ast = parse("test!\"'123 321'\"");
+
+		    assert.equals(ast, [{
+		        "op": "!",
+		        "text": "test!\"'123 321'\"",
+		        "raw": "test!'123 321'"
+		    }]);
+		},
 
 		"test!'123 321'.p1,.'p2 asd'" : function () {
 			var ast = parse("test!'123 321'.p1,.'p2 asd'");
